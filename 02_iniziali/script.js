2457a1adc552d3772debbdb26ebfe94073792cbb
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-08 15:33:45 +0200

    fix-console-log-02

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,10 +1,10 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-let initials = [];
 
 // Dichiara la funzione qui.
 function getInitials(namesList) {
+  const initials = [];
   for (let i = 0; i < namesList.length; i++) {
     let currentName = namesList[i];
     if (currentName) {
@@ -29,6 +29,6 @@ function getInitials(namesList) {
 // };
 
 // Invoca la funzione qui e stampa il risultato in console
-getInitials(names);
-console.log(initials);
+const InitialsName = getInitials(names);
+console.log(InitialsName);
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
