b9cdcca2c3902c64f247b5784225e63e7071b327
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-08 15:19:05 +0200

    fix-02

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,10 +1,10 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+let initials = [];
 
 // Dichiara la funzione qui.
 function getInitials(namesList) {
-  let initials = [];
   for (let i = 0; i < namesList.length; i++) {
     let currentName = namesList[i];
     if (currentName) {
