c694027655ec3869fd4a890819e800a881bd6b28
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-08 15:09:39 +0200

    add-02-iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,33 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+function getInitials(namesList) {
+  let initials = [];
+  for (let i = 0; i < namesList.length; i++) {
+    let currentName = namesList[i];
+    if (currentName) {
+      initials.push(currentName[0].toUpperCase());
+    }
+  }
 
+  return initials;
+}
+
+// Versione alternativa con arrow function
+
+// const getInitials = (namesList) => {
+//   let initials = [];
+//   for (let i = 0; i < namesList.length; i++) {
+//     let currentName = namesList[i];
+//     if (currentName) {
+//       initials.push(currentName[0].toUpperCase());
+//     }
+//   }
+//   return initials;
+// };
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+getInitials(names);
+console.log(initials);
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
